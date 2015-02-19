d97f5d8b1f7d0b24a4610edc1455b5b3eecf6afd
Steve Greatrex <stephen.greatrex@googlemail.com>
2015-02-19 15:10:36 +0000

    Reduced spacing in samples

--- samples/getting-entities.js
+++ samples/getting-entities.js
@@ -1,18 +1,18 @@
 /*global require:false*/
 
 (function(require) {
-	var request = require('./edge10-request');
+  var request = require('./edge10-request');
 
-	function getAllEntities() {
-		return request({
-			url: '/api/entity'
-		});
-	}
+  function getAllEntities() {
+    return request({
+      url: '/api/entity'
+    });
+  }
 
-	getAllEntities()
-		.then(function(entities) {
-			entities
-				.map(function(e) { return e.firstName + ' ' + e.lastName; })
-				.forEach(function(name) { console.log(name); })
-		});
+  getAllEntities()
+    .then(function(entities) {
+      entities
+        .map(function(e) { return e.firstName + ' ' + e.lastName; })
+        .forEach(function(name) { console.log(name); })
+    });
 }(require));
